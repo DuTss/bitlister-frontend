3eb166bc3e7f4cfe05c76f5462d429a426ff4a88
Emilien .D <emduthy@gmail.com>
2026-09-12 14:30:29 +0200

    composants Header et ListingList avec HTML et CSS séparés

---
+++ src/app/features/listings/listing-list/listing-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-list',
+  imports: [],
+  templateUrl: './listing-list.html',
+  styleUrl: './listing-list.css',
+})
+export class ListingList {}
